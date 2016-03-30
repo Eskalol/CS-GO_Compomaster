f2773034eaf6f82bb7111cb34ca2c79a08908431
Eska <eskil.nordland@gmail.com>
2016-03-31 01:02:46 +0200

    Finally the bracket works! TODO: some background styling!

--- src/compomaster/views/bracket/Bracket.java
+++ src/compomaster/views/bracket/Bracket.java
@@ -26,7 +26,6 @@ public class Bracket extends StackPane {
     private double deltaX;
     private double deltaY;
 
-
     /**
      * constructor
      *
@@ -36,6 +35,7 @@ public class Bracket extends StackPane {
         this.bracketSize = bracketSize;
         this.looserBracket = false;
     }
+
 
     /**
      * optional constructor for brakcet
@@ -47,6 +47,7 @@ public class Bracket extends StackPane {
         this.looserBracket = looserBracket;
     }
 
+
     /**
      * load method for Bracket
      */
@@ -55,6 +56,7 @@ public class Bracket extends StackPane {
         loadZoom();
         loadDrag();
         loadLoosersBracket();
+        joinWinnerAndLooserBracket();
     }
 
 
@@ -98,6 +100,7 @@ public class Bracket extends StackPane {
         });
     }
 
+
     /**
      * Method loads Winner bracket to stackPane
      */
@@ -105,6 +108,7 @@ public class Bracket extends StackPane {
         int nextPosX = 0;
         int nextPosY = 0;
         int bsize = bracketSize;
+
         for(int i = 0; i < (int)Math.pow(2, bsize); i++) {
             if(bsize > 0)
                 addHandVLinesWinner(nextPosX, nextPosY, i, (bracketSize - bsize));
@@ -114,6 +118,7 @@ public class Bracket extends StackPane {
                 i = -1;
                 //calc next y positon for first match in round
                 nextPosY =  (offsetY/2)+offsetY*((int)Math.pow(2, (bracketSize - bsize))-1);
+
                 //calc next x position
                 nextPosX += (this.looserBracket && bsize < bracketSize ? offsetX*2 : offsetX);
                 bsize--;
@@ -178,14 +183,18 @@ public class Bracket extends StackPane {
      */
     private void loadLoosersBracket() {
         if(!this.looserBracket) return;
-        int yStep = offsetY*((int)Math.pow(2, bracketSize)) + offsetY;
+
+        int yStep = offsetY*(int)Math.pow(2, bracketSize) + offsetY;
         int nextPosX = offsetX;
         int nextPosY = yStep;
         int bsize = bracketSize-1;
+
         for(int i = 0; i < (int)Math.pow(2, bsize); i++) {
+            addHandVLinesLooser(nextPosX, nextPosY, 0, i, bracketSize - bsize - 1);
+            addHandVLinesLooser(nextPosX+offsetX, nextPosY, 1, i, bracketSize - bsize - 1);
             addMatchRectangle(nextPosX, nextPosY);
             addMatchRectangle(nextPosX+offsetX, nextPosY);
-            nextPosY += ( bsize == bracketSize-1 ? offsetY : offsetY*((int)Math.pow(2, (bracketSize - bsize-1))) );
+            nextPosY += ( bsize == bracketSize-1 ? offsetY : offsetY*((int)Math.pow(2, (bracketSize - bsize - 1))) );
             if( (i + 1) == (int)Math.pow(2, bsize) ) {
                 i = -1;
                 //calc next y positon for first match in round
@@ -195,7 +204,99 @@ public class Bracket extends StackPane {
             }
         }
 
+        //Add grand final
+        addMatchRectangle(nextPosX, yStep-offsetY);
     }
+
+
+    /**
+     * method adds horizontal and vertical lines to Looser bracket
+     *
+     * @param x xPosition
+     * @param y yPosition
+     * @param matchType match type 0 is first column in looser bracket round 1 is second column
+     * @param matchRow match row in looserbracket round
+     * @param depth round/depth of tree
+     */
+    private void addHandVLinesLooser(int x, int y, int matchType, int matchRow, int depth) {
+        //calc length of horizontal line
+        int horLen = ( matchType == 0 ? 96 : 50);
+        int xOff = (matchType == 0 ? 168 : 145);
+
+        Line l1 = getHorizontalLine(horLen);
+        l1.setTranslateX(x+xOff);
+        l1.setTranslateY(y+12+lineThinckness);
+        this.getChildren().add(l1);
+
+        //first add lines for looser in winnersbracket round
+        if(matchType == 1 || depth == 0) {
+            Line l2 = getHorizontalLine(45);
+            l2.setTranslateX(x - 107);
+            l2.setTranslateY(y);
+            this.getChildren().add(l2);
+            Line l3 = getVerticalLine(10);
+            l3.setTranslateX(x - 130);
+            l3.setTranslateY(y - 5);
+            this.getChildren().add(l3);
+        }
+
+        //first round in looserbracket
+        if(depth == 0 && matchType == 0) {
+            Line l4 = getHorizontalLine(45);
+            l4.setTranslateX(x - 107);
+            l4.setTranslateY(y + 35);
+            this.getChildren().add(l4);
+            Line l5 = getVerticalLine(10);
+            l5.setTranslateX(x - 130);
+            l5.setTranslateY(y + 30);
+            this.getChildren().add(l5);
+        }
+
+        if(matchRow % 2 == 0 && matchType == 1 && depth+1 != bracketSize) {
+            int vLineLen = offsetY*((int)Math.pow(2, depth));
+            int yPos = y+(offsetY/2)+offsetY*((int)Math.pow(2, depth)-1)-vLineLen/2+62+lineThinckness;
+            Line l6 = getVerticalLine(vLineLen);
+            l6.setTranslateX(x+170);
+            l6.setTranslateY(yPos);
+            this.getChildren().add(l6);
+
+            Line l7 = getHorizontalLine(46);
+            l7.setTranslateX(x+170+23);
+            l7.setTranslateY(yPos);
+            this.getChildren().add(l7);
+        }
+
+    }
+
+    /**
+     * method joins winner and looser bracket together for grand final.
+     */
+    private void joinWinnerAndLooserBracket() {
+        if(!this.looserBracket) return;
+
+        //adding horisontal line for winner of winner bracket
+        int yPos = offsetY*(int)Math.pow(2, bracketSize)/2;
+        int xPos = offsetX*bracketSize*2-lineThinckness;
+        Line l1 = getHorizontalLine(offsetX+50);
+        l1.setTranslateX(xPos);
+        l1.setTranslateY(yPos-33);
+        this.getChildren().add(l1);
+        
+        //adding vertical line to join winner and looser bracket
+        xPos = offsetX*bracketSize*2+offsetX/2+20;
+        int w2Llen = offsetY*(int)Math.pow(2, bracketSize-1)/2 + offsetY*(int)Math.pow(2, bracketSize) + offsetY - yPos;
+        Line l2 = getVerticalLine(w2Llen);
+        l2.setTranslateX(xPos);
+        l2.setTranslateY(w2Llen/2+yPos-33);
+        this.getChildren().add(l2);
+
+        //line to grand final
+        Line l3 = getHorizontalLine(46);
+        l3.setTranslateX(xPos+23);
+        l3.setTranslateY(yPos*2+17);
+        this.getChildren().add(l3);
+    }
+
 
     /**
      * method return horizontal line with given length
